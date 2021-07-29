0e740e00e5b84946d7b3b686b1a02a86dcdcf9df
jungloul1 <jungloul1@gmail.com>
2021-07-29 16:25:23 +0100

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 //controllers : 
 const userController = require('./controllers/userControllers');
